5ad9e1ac2680fe700f8d39640c8b3ec831ac1ba6
Valentin Knabel <develop@vknabel.com>
2016-10-24 10:43:56 +0200

    Initial Commit

---
+++ ReactiveAu3dio/ReactiveAu3dio.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveAu3dio.h
+//  ReactiveAu3dio
+//
+//  Created by Valentin Knabel on 24.10.16.
+//  Copyright © 2016 Valentin Knabel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactiveAu3dio.
+FOUNDATION_EXPORT double ReactiveAu3dioVersionNumber;
+
+//! Project version string for ReactiveAu3dio.
+FOUNDATION_EXPORT const unsigned char ReactiveAu3dioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveAu3dio/PublicHeader.h>
+
+
